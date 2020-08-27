bd4d665445aec3d9921d87d96962356a05bc23a9
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-08-27 21:32:07 +0700

    Add Fiscal Year

--- app/tampilan/data/master/index.php
+++ app/tampilan/data/master/index.php
@@ -6,7 +6,7 @@
         <div class="card-content">
             <ul class="collection">
                 <a class="collection-item indigo-text" href="<?= basis_url('data/master/sekolah'); ?>">Sekolah <i class="material-icons right">chevron_right</i></a>
-                <a class="collection-item" href="<?= basis_url('data/master/kurikulum'); ?>">Kurikulum <i class="material-icons right">chevron_right</i></a>
+                <a class="collection-item indigo-text" href="<?= basis_url('data/master/kurikulum'); ?>">Kurikulum <i class="material-icons right">chevron_right</i></a>
                 <a class="collection-item" href="<?= basis_url('data/master/tapel'); ?>">Tahun Ajaran <i class="material-icons right">chevron_right</i></a>
                 <a class="collection-item" href="<?= basis_url('data/master/gedung'); ?>">Gedung <i class="material-icons right">chevron_right</i></a>
                 <a class="collection-item" href="<?= basis_url('data/master/ruangan'); ?>">Ruangan <i class="material-icons right">chevron_right</i></a>
